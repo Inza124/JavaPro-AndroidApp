94d972548396bb3d3934c75bc03190458dd49501
Inza124 <31655111+Inza124@users.noreply.github.com>
2018-06-19 11:53:38 +0200

    add ContentHelper

---
+++ app/src/main/java/com/example/piotr/javaproj/ContentHelper.java
@@ -0,0 +1,16 @@
+package com.example.piotr.javaproj;
+
+public class ContentHelper
+{
+    private static int whichLab;
+    private static int whichWyk;
+    private static int testPoints;
+
+    public int getWhichLab() {
+        return whichLab;
+    }
+
+    public void setWhichLab(int whichLab) {
+        this.whichLab = whichLab;
+    }
+}
